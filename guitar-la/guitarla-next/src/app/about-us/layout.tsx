4d1b61875a7cca777312ae1e5af9fa7555890feb
Fernando <lgarcia@tredicom.com>
2023-08-13 16:01:54 -0600

    refactor: :mag: Added SEO on about us layout

--- guitar-la/guitarla-next/src/app/about-us/layout.tsx
+++ guitar-la/guitarla-next/src/app/about-us/layout.tsx
@@ -1,6 +1,15 @@
+import { Metadata } from "next";
+
 interface LayoutPropsI {
   children: React.ReactNode;
 }
+
+/** @description Metadata for the SEO */
+export const metadata: Metadata = {
+  title: "GuitarLA - About us",
+  description: "About us, GuitarLA, music store",
+};
+
 const AboutUsLayout = ({ children }: LayoutPropsI) => {
   return (
     <>
